173d03533ea0d24306cb68189ed7187ec9687447
Aris-Breezy <root@localhost.localdomain>
2015-02-14 16:57:33 +0200

    Addthe encryption function

--- encbox.py
+++ encbox.py
@@ -12,6 +12,52 @@ if leng == 0:
     print("Try './encbox /path/to/file'\nQuiting!")
     sys.exit(1)
 
+
+def encrypt_file(key,in_filename, out_filename=None, chunksize=64*1024):
+    """ Encrypts a file using AES (CBC mode) with the
+        given key.
+
+        key:
+            The encryption key - a string that must be
+            either 16, 24 or 32 bytes long. Longer keys
+            are more secure.
+
+        in_filename:
+            Name of the input file
+
+        out_filename:
+            If None, '<in_filename>.enc' will be used.
+
+        chunksize:
+            Sets the size of the chunk which the function
+            uses to read and encrypt the file. Larger chunk
+            sizes can be faster for some files and machines.
+            chunksize must be divisible by 16.
+    """
+
+   
+    if not out_filename:
+        out_filename = in_filename + '.enc'
+
+    iv = ''.join(chr(random.randint(0, 0xFF)) for i in range(16))
+    encryptor = AES.new(key, AES.MODE_CBC, iv)
+    filesize = os.path.getsize(in_filename)
+
+    with open(in_filename, 'rb') as infile:
+        with open(out_filename, 'wb') as outfile:
+            outfile.write(struct.pack('<Q', filesize))
+            outfile.write(iv)
+
+            while True:
+                chunk = infile.read(chunksize)
+                if len(chunk) == 0:
+                    break
+                elif len(chunk) % 16 != 0:
+                    chunk += ' ' * (16 - len(chunk) % 16)
+
+                outfile.write(encryptor.encrypt(chunk))
+   #print "The new encrypted file is -->" ,out_filename
+
 #taking the app key and secret 
 key=raw_input('Enter your app key : ')
 secret=raw_input('Enter your app secret : ')
@@ -33,3 +79,6 @@ code = raw_input("Enter the authorization code here: ").strip()
 access_token,user_id= flow.finish(code)
 client = dropbox.client.DropboxClient(access_token)
 print 'The account has been linked successfully'
+
+
+
